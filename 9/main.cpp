bba50c66c3401d9b0949ba456d273b83fafa99fd
Theodore <elvisxiaozhi@gmail.com>
2018-07-29 20:04:44 +0800

    add solved problem 9

---
+++ 9/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+bool isPalindrome(int x)
+{
+    if(x < 0) {
+        return false;
+    }
+
+    int num = x;
+    int digits = 0;
+    while(num > 0) {
+        num /= 10;
+        ++digits;
+    }
+
+    vector<int> remainderVec;
+    for(int i = 0; i < digits; ++i) {
+        int remainder = 0;
+        if(i == 0) {
+            remainder = x % 10;
+        }
+        else {
+            remainder = ((x / (int)pow(10, i)) % 10);
+        }
+        remainderVec.push_back(remainder);
+    }
+
+    int reversedNum = 0;
+    for(int i = digits; i > 0; --i) {
+        reversedNum += (remainderVec[digits - i] * (int)pow(10, i - 1));
+    }
+
+    if(reversedNum == x) {
+        return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    cout << isPalindrome(121) << endl;
+    return 0;
+}
