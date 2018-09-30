05d860047ccd730d20f4d66a503a8d0ea54dea93
Theodore <elvisxiaozhi@gmail.com>
2018-09-30 20:22:23 +0800

    add solved problem 53

--- 53/main.cpp
+++ 53/main.cpp
@@ -6,27 +6,7 @@ using namespace std;
 
 int maxSubArray(vector<int>& nums)
 {
-    int sum = numeric_limits<int>::min();
-
-    for(unsigned int i = 0; i < nums.size(); ++i) {
-        if(sum < nums[i]) {
-            sum = nums[i];
-        }
-        int tempSum = nums[i];
-        for(unsigned int j = i + 1; j < nums.size(); ++j) {
-            tempSum += nums[j];
-            if(tempSum > sum) {
-                sum = tempSum;
-            }
-        }
-    }
-
-    return sum;
-}
-
-int maxSubArray2(vector<int>& nums) //using the divide and conquer approach
-{
-    int sum = numeric_limits<int>::min();
+    int sum = INT_MIN;
 
     for(unsigned int i = 0; i < nums.size(); ++i) {
         if(sum < nums[i]) {
@@ -46,7 +26,7 @@ int maxSubArray2(vector<int>& nums) //using the divide and conquer approach
 
 int main()
 {
-    vector<int> vec1 = {-2,1,-3,4,-1,2,1,-5,4};
+    vector<int> vec1 = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     vector<int> vec2 = {-2, -5, 6, -2, -3, 1, 5, -6};
     vector<int> vec3 = {-1};
     cout << maxSubArray(vec1) << endl;
